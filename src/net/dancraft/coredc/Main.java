613bb6d18351b6f63412bfa74a1c04968440d5ba
DansTooGood <dancraftserver@outlook.com>
2014-09-27 14:50:11 +0100

    Remove Anvil Durability

--- src/net/dancraft/coredc/Main.java
+++ src/net/dancraft/coredc/Main.java
@@ -2,12 +2,17 @@ package net.dancraft.coredc;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.massivecraft.factions.entity.UPlayer;
@@ -36,13 +41,21 @@ public class Main extends JavaPlugin implements Listener {
 		e.setFormat(ChatColor.translateAlternateColorCodes('&', getFaction(player) + "&7" + e.getPlayer().getName() + "&a: &7" + e.getMessage()));
 	}
 
+	@EventHandler
+	public void playerAnvil(PlayerInteractEvent e) {
+		if (e.getClickedBlock().getType() == Material.ANVIL){
+			Block block = e.getClickedBlock();
+			block.setData((byte)0);
+		}
+	}
+
 	public String getFaction(Player player) {
 		String message = "";
 		FileConfiguration config = plugin.getConfig();
 		if (config.getBoolean("Factions.State")) {
 			Player p = Bukkit.getPlayerExact(player.getName());
 			UPlayer uplayer = UPlayer.get(p);
-			if(uplayer.hasFaction()){
+			if (uplayer.hasFaction()) {
 				message = ChatColor.translateAlternateColorCodes('&', "&a[&7" + uplayer.getFactionName() + "&a] ");
 				return message;
 			} else {
